ab6dd240ab97b228be5476a38caf538848d7a0e7
Alexander Kremeshnoi <alexander.kremeshnoi@gmail.com>
2020-12-22 19:56:51 +0200

    fix about text

--- dev/src/components/About.vue
+++ dev/src/components/About.vue
@@ -4,7 +4,7 @@
 			About me
 		</h2>
 		<p class="about__description">
-			I am currently freelancing as a front-end web developer, which brings me great pleasure. I’m not afraid
+			I am currently freelancing as a front-end developer, which brings me great pleasure. I’m not afraid
 			of new challenges and am always ready to develop my professional and human qualities. Outside of work, I
 			enjoy spending time with friends and pursuing a few hobbies. Check out my work and feel free to get in
 			touch with me, I’m always happy to help!
