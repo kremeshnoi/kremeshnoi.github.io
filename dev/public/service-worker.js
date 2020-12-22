823861b05aed4d12ae96c648d864d539d16a288e
Alexander Kremeshnoi <alexander.kremeshnoi@gmail.com>
2020-12-22 22:30:08 +0200

    setup config to generate manifest.json

---
+++ dev/public/service-worker.js
@@ -0,0 +1,42 @@
+importScripts("/precache-manifest.f8e91a7f395fb0fb8f79ad8ff51b2c19.js", "https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+if (workbox) {
+	console.log(`Workbox is loaded`);
+
+	workbox.precaching.precacheAndRoute(self.__precacheManifest);
+
+}
+else {
+	console.log(`Workbox didn't load`);
+}
+
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/precache-manifest.72ea412775040c03f1554f85df37e00c.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "kremeshoi"});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
--- dev/vue.config.js
+++ dev/vue.config.js
@@ -1,15 +1,17 @@
 const webpack = require('webpack');
+const { WebpackManifestPlugin } = require('webpack-manifest-plugin');
 
 module.exports = {
   configureWebpack: {
     plugins: [
       new webpack.optimize.LimitChunkCountPlugin({
         maxChunks: 6
-      })
+      }),
+      new WebpackManifestPlugin()
     ]
   },
   pwa: {
-    name: 'kremeshnoi',
+    name: 'kremeshnoi'
   },
   css: {
     sourceMap: process.env.NODE_ENV !== 'production'
