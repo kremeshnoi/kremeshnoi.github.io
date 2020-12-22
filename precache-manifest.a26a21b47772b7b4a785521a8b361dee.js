799835c9cf903c59d4bcd64fca72fa1e856b24e1
Alexander Kremeshnoi <alexander.kremeshnoi@gmail.com>
2020-12-22 23:20:30 +0200

    rebuild project

--- precache-manifest.a26a21b47772b7b4a785521a8b361dee.js
+++ precache-manifest.a26a21b47772b7b4a785521a8b361dee.js
@@ -1,31 +1,27 @@
 self.__precacheManifest = [
   {
-    "revision": "cf15b118df30c2f9ee68",
-    "url": "/css/app.23eb6370.css"
+    "revision": "934bed554f51ba99d72c",
+    "url": "/css/app.adc488e1.css"
   },
   {
-    "revision": "cf15b118df30c2f9ee68",
-    "url": "/js/app.52dab31d.js"
+    "revision": "934bed554f51ba99d72c",
+    "url": "/js/app.6436d138.js"
   },
   {
     "revision": "cc23a7b5c5665aff7540",
     "url": "/js/chunk-vendors.eb061ca7.js"
   },
   {
-    "revision": "ea5eda4766033a1309db9e0d0bbf037d",
+    "revision": "d224a58a32200b1ca7222576d61e4b4c",
     "url": "/index.html"
   },
   {
-    "revision": "716367dc62f1470aca4c9de37312192c",
+    "revision": "cf973727bfe81647d528d5f1f0a2d6ab",
     "url": "/favicon.png"
   },
   {
     "revision": "01425a965c0552190de143bbc97191be",
     "url": "/img/iapple.jpg"
-  },
-  {
-    "revision": "5de18ae4e6defdedadf20af6bd0b252e",
-    "url": "/img/marta-deco.jpg"
   },
   {
     "revision": "99d3135cca539af6e344e8a4fbd8cd04",
@@ -34,6 +30,14 @@ self.__precacheManifest = [
   {
     "revision": "fc1d5041828c971c9e5558ed658b6b7d",
     "url": "/img/photo.jpg"
+  },
+  {
+    "revision": "5de18ae4e6defdedadf20af6bd0b252e",
+    "url": "/img/marta-deco.jpg"
+  },
+  {
+    "revision": "763dfb435645bef9b3d963d9bdbca15e",
+    "url": "/service-worker.js"
   },
   {
     "revision": "0d41db2c724f9a3dfefb58dc292a9f55",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.72ea412775040c03f1554f85df37e00c.js"
+  "/precache-manifest.a26a21b47772b7b4a785521a8b361dee.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "kremeshoi"});
