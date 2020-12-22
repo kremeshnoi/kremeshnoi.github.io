c2ac7df84c437790e2d8f67638ae24820eb37272
Alexander Kremeshnoi <alexander.kremeshnoi@gmail.com>
2020-12-23 00:44:03 +0200

    rebuild project

--- precache-manifest.c167ec55fae952d4839b1c01363c0e47.js
+++ precache-manifest.c167ec55fae952d4839b1c01363c0e47.js
@@ -1,18 +1,18 @@
 self.__precacheManifest = [
   {
-    "revision": "934bed554f51ba99d72c",
-    "url": "/css/app.adc488e1.css"
+    "revision": "78319143574405a45096",
+    "url": "/css/app.7ea74da1.css"
   },
   {
-    "revision": "934bed554f51ba99d72c",
-    "url": "/js/app.6436d138.js"
+    "revision": "78319143574405a45096",
+    "url": "/js/app.18351727.js"
   },
   {
     "revision": "cc23a7b5c5665aff7540",
     "url": "/js/chunk-vendors.eb061ca7.js"
   },
   {
-    "revision": "d224a58a32200b1ca7222576d61e4b4c",
+    "revision": "3b8d3a8bd71a869f3a8f4b0aed2f2cca",
     "url": "/index.html"
   },
   {
@@ -28,20 +28,20 @@ self.__precacheManifest = [
     "url": "/img/oldportfolio.jpg"
   },
   {
-    "revision": "fc1d5041828c971c9e5558ed658b6b7d",
-    "url": "/img/photo.jpg"
-  },
-  {
     "revision": "5de18ae4e6defdedadf20af6bd0b252e",
     "url": "/img/marta-deco.jpg"
   },
   {
-    "revision": "763dfb435645bef9b3d963d9bdbca15e",
-    "url": "/service-worker.js"
+    "revision": "fc1d5041828c971c9e5558ed658b6b7d",
+    "url": "/img/photo.jpg"
   },
   {
     "revision": "0d41db2c724f9a3dfefb58dc292a9f55",
     "url": "/img/otaku-library.jpg"
+  },
+  {
+    "revision": "763dfb435645bef9b3d963d9bdbca15e",
+    "url": "/service-worker.js"
   },
   {
     "revision": "221664bb103c8803a41e616a64b4d9a0",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a26a21b47772b7b4a785521a8b361dee.js"
+  "/precache-manifest.c167ec55fae952d4839b1c01363c0e47.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "kremeshoi"});
