96e121b80b8578121b2bdc6febcbd754753efa09
Alexander Kremeshnoi <alexander.kremeshnoi@gmail.com>
2020-12-23 20:12:12 +0200

    rebuild project

---
+++ precache-manifest.40f96e6d6036e5acf65b3ff36d562526.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "68ac17218762f762a76a",
+    "url": "/css/app.c74b6d49.css"
+  },
+  {
+    "revision": "68ac17218762f762a76a",
+    "url": "/js/app.e3582862.js"
+  },
+  {
+    "revision": "b6dd01ded96c6b840c56",
+    "url": "/js/chunk-vendors.fc2c6a7e.js"
+  },
+  {
+    "revision": "19ef6072f4a71c058a218fd98e1d3b27",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01425a965c0552190de143bbc97191be",
+    "url": "/img/iapple.jpg"
+  },
+  {
+    "revision": "cf973727bfe81647d528d5f1f0a2d6ab",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "5f09c7a8f8134722305aa60294afc26f",
+    "url": "/img/current-portfolio.jpg"
+  },
+  {
+    "revision": "99d3135cca539af6e344e8a4fbd8cd04",
+    "url": "/img/old-portfolio.jpg"
+  },
+  {
+    "revision": "5de18ae4e6defdedadf20af6bd0b252e",
+    "url": "/img/marta-deco.jpg"
+  },
+  {
+    "revision": "fc1d5041828c971c9e5558ed658b6b7d",
+    "url": "/img/photo.jpg"
+  },
+  {
+    "revision": "763dfb435645bef9b3d963d9bdbca15e",
+    "url": "/service-worker.js"
+  },
+  {
+    "revision": "221664bb103c8803a41e616a64b4d9a0",
+    "url": "/img/talkable.jpg"
+  },
+  {
+    "revision": "0d41db2c724f9a3dfefb58dc292a9f55",
+    "url": "/img/otaku-library.jpg"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,29 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/precache-manifest.40f96e6d6036e5acf65b3ff36d562526.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "kremeshoi"});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
