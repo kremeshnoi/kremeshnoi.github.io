9f05e43b7e609346c4a9fb8c7700e2473dfc6fe1
Alexander Kremeshnoi <alexander.kremeshnoi@gmail.com>
2020-12-24 01:14:26 +0200

    rebuild project

---
+++ precache-manifest.7a266223373e8991cc526c2cf6322238.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = [
+  {
+    "revision": "6a187f97f892207672e2",
+    "url": "/css/app.5949a8fe.css"
+  },
+  {
+    "revision": "6a187f97f892207672e2",
+    "url": "/js/app.cb03d8ce.js"
+  },
+  {
+    "revision": "b6dd01ded96c6b840c56",
+    "url": "/js/chunk-vendors.fc2c6a7e.js"
+  },
+  {
+    "revision": "f59d41c9c2c700bdfbe1774f97af12f1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf973727bfe81647d528d5f1f0a2d6ab",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "5f09c7a8f8134722305aa60294afc26f",
+    "url": "/img/current-portfolio.jpg"
+  },
+  {
+    "revision": "9675d5ec8a6d04c55d73fdf397aef3da",
+    "url": "/img/fl.ru-icon.jpg"
+  },
+  {
+    "revision": "3b6bf81e624e4c69e00a0430cb4da059",
+    "url": "/img/freelancer-icon.jpg"
+  },
+  {
+    "revision": "01425a965c0552190de143bbc97191be",
+    "url": "/img/iapple.jpg"
+  },
+  {
+    "revision": "5de18ae4e6defdedadf20af6bd0b252e",
+    "url": "/img/marta-deco.jpg"
+  },
+  {
+    "revision": "14c08c90e3aee8ba6bae124e8a0ef89b",
+    "url": "/img/nlau-icon.jpg"
+  },
+  {
+    "revision": "99d3135cca539af6e344e8a4fbd8cd04",
+    "url": "/img/old-portfolio.jpg"
+  },
+  {
+    "revision": "f0f78bfb0a949b912ae78ec40fd95198",
+    "url": "/img/product-cards.jpg"
+  },
+  {
+    "revision": "fc1d5041828c971c9e5558ed658b6b7d",
+    "url": "/img/photo.jpg"
+  },
+  {
+    "revision": "586f185c1ea3a82cfda327ee6494d8a5",
+    "url": "/img/talkable-icon.jpg"
+  },
+  {
+    "revision": "0d41db2c724f9a3dfefb58dc292a9f55",
+    "url": "/img/otaku-library.jpg"
+  },
+  {
+    "revision": "221664bb103c8803a41e616a64b4d9a0",
+    "url": "/img/talkable.jpg"
+  },
+  {
+    "revision": "763dfb435645bef9b3d963d9bdbca15e",
+    "url": "/service-worker.js"
+  }
+];
