8c02bd925f457163e7aecee6cbf939ed944fcdb1
Alexander Kremeshnoi <alexander.kremeshnoi@gmail.com>
2020-12-23 15:49:40 +0200

    remove unnecessary stuff from App.vue

--- dev/src/App.vue
+++ dev/src/App.vue
@@ -1,23 +1,12 @@
 <template>
+
 	<div id="app">
 		<main>
-			<fade-transition :duration="250" mode="out-in" origin="center">
 				<router-view/>
-			</fade-transition>
 		</main>
 	</div>
+
 </template>
-<script>
-
-import { FadeTransition } from "vue2-transitions";
-
-	export default {
-		components: {
-			FadeTransition
-		}
-	};
-
-</script>
 
 <style>
 
