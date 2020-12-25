4743fa8851e7eb459f34bec361e9fb81010feb25
Alexander Kremeshnoi <alexander.kremeshnoi@gmail.com>
2020-12-25 12:51:47 +0200

    rebuild project

--- precache-manifest.e17f15daa811120a378787e579958560.js
+++ precache-manifest.e17f15daa811120a378787e579958560.js
@@ -1,19 +1,23 @@
 self.__precacheManifest = [
   {
-    "revision": "eb67c37b9a53c2687a73",
-    "url": "/css/app.9308db28.css"
+    "revision": "683d6ec443e8fac9558a",
+    "url": "/css/app.452b0907.css"
   },
   {
-    "revision": "eb67c37b9a53c2687a73",
-    "url": "/js/app.5c9b6496.js"
+    "revision": "683d6ec443e8fac9558a",
+    "url": "/js/app.d32c8f26.js"
   },
   {
     "revision": "b6dd01ded96c6b840c56",
     "url": "/js/chunk-vendors.fc2c6a7e.js"
   },
   {
-    "revision": "7e7297cdd20e7ae50429d6d3c254a490",
+    "revision": "eb4ec9594575b20651ec84b88b4b0328",
     "url": "/index.html"
+  },
+  {
+    "revision": "cf973727bfe81647d528d5f1f0a2d6ab",
+    "url": "/favicon.png"
   },
   {
     "revision": "9675d5ec8a6d04c55d73fdf397aef3da",
@@ -24,10 +28,6 @@ self.__precacheManifest = [
     "url": "/img/current-portfolio.jpg"
   },
   {
-    "revision": "cf973727bfe81647d528d5f1f0a2d6ab",
-    "url": "/favicon.png"
-  },
-  {
     "revision": "3b6bf81e624e4c69e00a0430cb4da059",
     "url": "/img/freelancer-icon.jpg"
   },
@@ -36,28 +36,32 @@ self.__precacheManifest = [
     "url": "/img/iapple.jpg"
   },
   {
-    "revision": "14c08c90e3aee8ba6bae124e8a0ef89b",
-    "url": "/img/nlau-icon.jpg"
+    "revision": "fc1d5041828c971c9e5558ed658b6b7d",
+    "url": "/img/photo.jpg"
   },
   {
     "revision": "99d3135cca539af6e344e8a4fbd8cd04",
     "url": "/img/old-portfolio.jpg"
   },
   {
-    "revision": "f0f78bfb0a949b912ae78ec40fd95198",
-    "url": "/img/product-cards.jpg"
-  },
-  {
-    "revision": "fc1d5041828c971c9e5558ed658b6b7d",
-    "url": "/img/photo.jpg"
-  },
-  {
-    "revision": "586f185c1ea3a82cfda327ee6494d8a5",
-    "url": "/img/talkable-icon.jpg"
+    "revision": "14c08c90e3aee8ba6bae124e8a0ef89b",
+    "url": "/img/nlau-icon.jpg"
   },
   {
     "revision": "5de18ae4e6defdedadf20af6bd0b252e",
     "url": "/img/marta-deco.jpg"
+  },
+  {
+    "revision": "f0f78bfb0a949b912ae78ec40fd95198",
+    "url": "/img/product-cards.jpg"
+  },
+  {
+    "revision": "763dfb435645bef9b3d963d9bdbca15e",
+    "url": "/service-worker.js"
+  },
+  {
+    "revision": "586f185c1ea3a82cfda327ee6494d8a5",
+    "url": "/img/talkable-icon.jpg"
   },
   {
     "revision": "0d41db2c724f9a3dfefb58dc292a9f55",
@@ -66,9 +70,5 @@ self.__precacheManifest = [
   {
     "revision": "221664bb103c8803a41e616a64b4d9a0",
     "url": "/img/talkable.jpg"
-  },
-  {
-    "revision": "763dfb435645bef9b3d963d9bdbca15e",
-    "url": "/service-worker.js"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.b9c237d01c3d33404783f38010f65ff5.js"
+  "/precache-manifest.e17f15daa811120a378787e579958560.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "kremeshoi"});
